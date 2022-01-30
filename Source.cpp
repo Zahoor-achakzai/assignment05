761535161818d100b9e31d500c109c689aa158c5
Zahoor-achakzai <97626726+Zahoor-achakzai@users.noreply.github.com>
2022-01-30 15:32:47 +0500

    Revert "Addfunction"

--- Source.cpp
+++ Source.cpp
@@ -3,10 +3,17 @@
 using namespace std;
 class contactinfo {
 private:
+	string name;
+	double cnic;
 	double mbno;
 	string address;
 public:
-	
+	string getname() {
+		return name;
+	}
+	double getcnic() {
+		return cnic;
+	}
 	double getmobileno()
 	{
 		return mbno;
@@ -15,9 +22,17 @@ public:
 	{
 		return address;
 	}
+	void setname(string n)
+	{
+		name = n;
+	}
 	void setaddress(string a)
 	{
 		address = a;
+	}
+	void setcnic(double c)
+	{
+		cnic = c;
 	}
 	void setmobileno(double n)
 	{
@@ -26,44 +41,19 @@ public:
 	void disp()
 	{
 		cout << "\tinfo " << endl;
+		cout << "name = " << name << endl;
+		cout << "cnic = " << cnic << endl;
 		cout << "mbno = " << mbno << endl;
 		cout << "address = " << address << endl;
 	}
 
 };
-class person:contactinfo
-{
-	string name;
-	double cnic;
-	person()
-	{
-		name = " ";
-		cnic = 0;
-	}
-	string getname() {
-		return name;
-	}
-	double getcnic() {
-		return cnic;
-	}
-	void setname(string n)
-	{
-		name = n;
-	}
-	void setcnic(double c)
-	{
-		cnic = c;
-	}
-	void disp()
-	{
-		cout << "name = " << name << endl;
-		cout << "cnic = " << cnic << endl;
-	}
-};
 void adddata(contactinfo obj)
 {
+	
 	obj.setname("Zahoor Ahmed");
 	obj.setaddress("XYZ");
+	obj.setcnic(23656622345);
 	obj.setmobileno(023564546);
 	obj.disp();
 }
